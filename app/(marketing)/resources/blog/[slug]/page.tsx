345c40c7438cecd3cda9f99ff46b4b05a1bcb7f6
Bourokba Aymen <153782307+aymenbkb@users.noreply.github.com>
2025-09-06 23:51:46 +0200

    Fix for Vercel

--- app/(marketing)/resources/blog/[slug]/page.tsx
+++ app/(marketing)/resources/blog/[slug]/page.tsx
@@ -1,26 +1,32 @@
-import React from 'react'
 import blogs from "@/utils/constants/blogs.json";
-interface Props {
-    params: {
-        slug: string
-    }
+
+interface PageProps {
+  params: {
+    slug: string;
+  };
 }
 
-const BlogPage = ({ params }: Props) => {
+export default function BlogPage({ params }: PageProps) {
+  const blog = blogs.find((blog) => blog.slug === params.slug);
 
-    const blog = blogs.find((blog) => blog.slug === params.slug);
+  if (!blog) {
     return (
-        <div className="flex flex-col items-center justify-center max-w-6xl mx-auto px-4 md:px-0 pb-80">
-            <div className="flex flex-col items-center justify-center">
-                <h1 className="text-2xl md:text-4xl lg:text-5xl font-semibold font-heading text-center mt-6 !leading-tight">
-                    {blog?.title}
-                </h1>
-                <p className="text-base md:text-lg mt-6 text-center text-muted-foreground">
-                    {blog?.description}
-                </p>
-            </div>
-        </div>
-    )
-};
+      <div className="flex flex-col items-center justify-center max-w-6xl mx-auto px-4 md:px-0 pb-80">
+        <h1 className="text-2xl font-semibold mt-6">Blog not found</h1>
+      </div>
+    );
+  }
 
-export default BlogPage
+  return (
+    <div className="flex flex-col items-center justify-center max-w-6xl mx-auto px-4 md:px-0 pb-80">
+      <div className="flex flex-col items-center justify-center">
+        <h1 className="text-2xl md:text-4xl lg:text-5xl font-semibold font-heading text-center mt-6 !leading-tight">
+          {blog.title}
+        </h1>
+        <p className="text-base md:text-lg mt-6 text-center text-muted-foreground">
+          {blog.description}
+        </p>
+      </div>
+    </div>
+  );
+}
