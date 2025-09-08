18b02c0a6ac7a272d2236fb7b6f173b763c95af5
Bourokba Aymen <153782307+aymenbkb@users.noreply.github.com>
2025-09-08 18:16:16 +0200

    replora

--- middleware.ts
+++ middleware.ts
@@ -1,36 +1,32 @@
-import { authMiddleware } from '@clerk/nextjs';
-import { NextResponse } from 'next/server';
+import { clerkMiddleware } from "@clerk/nextjs/server";
+import { NextResponse } from "next/server";
 
-export default authMiddleware({
-  publicRoutes: [
-    '/',
-    '/api/oauth/meta',
-    '/api/oauth/telegram',
-    '/marketing',
-    '/privacy',
-    '/terms',
-    '/auth/sign-in(.*)',
-    '/auth/sign-up(.*)'
-  ],
-  afterAuth(auth, req) {
-    // Handle users who aren't authenticated
-    if (!auth.userId && !auth.isPublicRoute) {
-      const signInUrl = new URL('/auth/sign-in', req.url);
-      signInUrl.searchParams.set('redirect_url', req.url);
-      return NextResponse.redirect(signInUrl);
-    }
-    
-    // If user is signed in and the route is /, redirect to /dashboard
-    if (auth.userId && req.nextUrl.pathname === '/') {
-      const dashboardUrl = new URL('/dashboard', req.url);
-      return NextResponse.redirect(dashboardUrl);
-    }
-    
-    return NextResponse.next();
+export default clerkMiddleware((auth, req) => {
+  const { userId, isPublicRoute } = auth() as any;
+  const { pathname } = req.nextUrl;
+
+  // Handle users who aren't authenticated
+  if (!userId && !isPublicRoute) {
+    const signInUrl = new URL("/auth/sign-in", req.url);
+    signInUrl.searchParams.set("redirect_url", req.url);
+    return NextResponse.redirect(signInUrl);
   }
+
+  // If user is signed in and the route is /
+  if (userId && pathname === "/") {
+    const dashboardUrl = new URL("/dashboard", req.url);
+    return NextResponse.redirect(dashboardUrl);
+  }
+
+  return NextResponse.next();
 });
 
 export const config = {
-  runtime: 'nodejs',
-  matcher: ['/((?!.+\\.[\\w]+$|_next).*)', '/', '/(api|trpc)(.*)'],
+  runtime: "nodejs",
+  matcher: [
+    // Exclude static files and Next.js internals
+    "/((?!_next|.*\\..*).*)",
+    "/",
+    "/(api|trpc)(.*)",
+  ],
 };
